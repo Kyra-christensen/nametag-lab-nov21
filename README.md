2f3904952819544f0d8f4d6bdd648b90239803c8
Kyra Christensen <christensen.kyral@gmail.com>
2021-11-04 10:46:04 -0700

    created pronoun selection and looping color button

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nametag-lab-nov21
+
+<!-- nameInput.addEventListener('keyup',
+    function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            const newName = nameInput.value;
+            nameTag.textContent = newName;
+            nameInput.value = '' -->
--- app.js
+++ app.js
@@ -3,20 +3,31 @@ const nameInputEl = document.getElementById('name-input');
 const nameDisplayEl = document.getElementById('name-display');
 const colorButtonEl = document.getElementById('color-button');
 const boxEl = document.getElementById('box');
+const colors = ['pink', 'red', 'orange', 'yellow', 'blue', 'green'];
+let colorNumber = 0;
+const proNouns = document.getElementById('pronouns');
+const pronounDisplay = document.getElementById('pronoun-display');
 
 console.log(updateButtonEl);
 console.log(nameInputEl);
 console.log(nameDisplayEl);
 console.log(colorButtonEl);
 console.log(boxEl);
+console.log(proNouns);
+console.log(pronounDisplay);
 
 updateButtonEl.addEventListener('click', () => {
     const newName = nameInputEl.value;
     nameDisplayEl.textContent = newName;
     console.log('you clicked the button,' + newName + '.');
-});
+
+    nameInputEl.value= '';
+    
+    pronounDisplay.textContent = proNouns.value;
+})    
 
 colorButtonEl.addEventListener('click', () => {
     console.log('You clicked the color button');
-    boxEl.style.backgroundColor = 'pink';
+    boxEl.style.backgroundColor = colors[colorNumber % colors.length];
+    colorNumber++;
 })
